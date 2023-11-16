ecda2cc5ff664bb89d28b9bd083eb21b7ffd4304
ahoang2502 <athoang252@gmail.com>
2023-11-16 23:56:43 +1100

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 
 export default function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+		<main className="bg-slate-300/20">
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={"Home"} />
+					<Route path="/about" element={"About"} />
+					<Route path="/projects" element={"Projects"} />
+					<Route path="/contact" element={"Contact"} />
+				</Routes>
+			</Router>
+		</main>
+	);
 }
