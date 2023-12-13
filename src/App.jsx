e93ee67b05390651da38e3c5138b65cbe390de27
ahoang2502 <athoang252@gmail.com>
2023-12-13 23:07:35 +1100

    projects-page

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import ContactPage from "./pages/ContactPage";
 
 export default function App() {
 	return (
-		<main className="bg-slate-300/20 h-[100%]">
+		<main className="bg-slate-300/20 h-full">
 			<Router>
 				<Navbar />
 				<Routes>
